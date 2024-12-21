8ccbda77c129fab4db3587b9564c1080b3027680
EL MABROUK Marouane <elmabroukmarouane@gmail.com>
2024-12-21 02:16:44 +0100

    Implement Search Videos Module

---
+++ YoutubeChaineVideos.Client.Busines/Services/Interface/IVideoService.cs
@@ -0,0 +1,12 @@
+﻿using YoutubeChaineVideos.Client.Domain.Models;
+using YoutubeChaineVideos.Client.Domain.Models.Requests;
+
+namespace YoutubeChaineVideos.Client.Busines.Services.Interface
+{
+    public interface IVideoService
+    {
+        #region SEARCH AND ADD YOUTUBE VIDEOS IN DB
+        Task<VideoViewModel?> SearchAddVideos(string uri, string? token, SearchAddVideosRequest searchAddVideosRequest);
+        #endregion
+    }
+}
---
+++ YoutubeChaineVideos.Client.Domain/Models/Requests/SearchAddVideosRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace YoutubeChaineVideos.Client.Domain.Models.Requests
+{
+    public class SearchAddVideosRequest
+    {
+        public required int ChannelId { get; set; }
+        public required string QuerySearch { get; set; }
+        public required int MaxResults { get; set; }
+        public bool GetRecentVideos { get; set; } = false;
+        public string VideoType { get; set; } = "video";
+    }
+}
